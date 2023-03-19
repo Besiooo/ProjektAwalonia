98b03521de6591662c87a7cdf8788b7994bb2d12
dsalachna <dsalachna020202@outlook.com>
2023-03-19 13:25:43 +0100

    Initial commit

---
+++ AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace AvaloniaApplication2.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+    public static string TimeString => GetTime();
+
+    static string GetTime()
+    {
+        while (true)
+        {
+            DateTime currentTime;
+            currentTime = DateTime.Now;
+            Thread.Sleep(1000);
+            return currentTime.ToString("hh:mm:ss");
+        }
+    }
+}
